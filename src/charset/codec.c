a57f3931e853ba31f3f397e055b1445f627c8bcb
John Mark Bell <jmb@netsurf-browser.org>
2008-09-04 17:02:14 +0000

    Extended 8bit coded. Mostly for Windows-125n support. Also needs testing.
    svn path=/trunk/libparserutils/; revision=5235

--- src/charset/codec.c
+++ src/charset/codec.c
@@ -15,6 +15,7 @@ extern parserutils_charset_handler iconv_codec_handler;
 #endif
 
 extern parserutils_charset_handler charset_8859_codec_handler;
+extern parserutils_charset_handler charset_ext8_codec_handler;
 extern parserutils_charset_handler charset_utf8_codec_handler;
 extern parserutils_charset_handler charset_utf16_codec_handler;
 
@@ -22,6 +23,7 @@ static parserutils_charset_handler *handler_table[] = {
 	&charset_utf8_codec_handler,
 	&charset_utf16_codec_handler,
 	&charset_8859_codec_handler,
+	&charset_ext8_codec_handler,
 #ifdef WITH_ICONV_CODEC
 	&iconv_codec_handler,
 #endif
